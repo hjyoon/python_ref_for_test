f8223c36e361f33fd62003c0e33d90822aae63de
Hyojeon Yoon <se1620236@naver.com>
2021-07-05 22:46:12 +0900

    Update time_test.py

--- time_test.py
+++ time_test.py
@@ -1,6 +1,4 @@
 import time
-import datetime
-import calendar
 
 # 1970년 1월 1일 0시 0분 0초 이후 경과한 시간을 초단위로 반환
 # 시간대는 UTC(Universal Time Coordinated, 협정 세계시)를 사용
@@ -14,3 +12,15 @@ print(time.localtime(time.time()))
 
 # time.localtime으로 만든 객체는 time.strftime 함수를 사용하여 원하는 날짜/시간 포맷으로 출력
 print(time.strftime('%Y-%m-%d', time.localtime(time.time())))
+print(time.strftime('%Y-%m-%d %I:%M:%S %p', time.localtime(time.time())))
+
+# strptime() 함수는 strftime() 함수와 정반대로 특정 포멧의 문자열을 time_struct 타입으로 변환
+string = '2019-11-30 02:35:26 PM'
+tm = time.strptime(string, '%Y-%m-%d %I:%M:%S %p')
+print(tm)
+
+# 주어진 timestamp를 현지 시간대 기준으로 소위 미국에서 흔히 사용되는 (요 월 일 시:분:초 년) 포멧으로 변환
+print(time.ctime(time.time())) # time.asctime() 과 같음
+
+# sleep() 함수는 프로그램의 실행을 일정 시간동안 지연시키고 싶을 때 사용. 지연시키고 싶은 시간을 초단위로 넘기면, 그 시간동안 프로그램의 실행이 멈췄다가 다시 실행
+# time.sleep(1.5) # 1.5 초간 지연
