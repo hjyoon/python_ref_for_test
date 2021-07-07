837aa95b35b0f263f18fe94f6710b6e6cdf5dc2a
Hyojeon Yoon <se1620236@naver.com>
2021-07-08 01:08:02 +0900

    Update date_time_test.py

--- date_time_test.py
+++ date_time_test.py
@@ -5,8 +5,12 @@ print(datetime.timedelta(days=5, seconds=63000))
 print(datetime.timedelta(weeks=7))
 
 print(datetime.date.today())
+print(datetime.date.today().replace(year=2020))
 
 datetime.timezone(datetime.timedelta(hours=9))
 
 print(datetime.date(2019, 12, 25))
 print(datetime.date.fromisoformat('2019-12-25'))
+
+print(datetime.time(13, 42, 35))
+print(datetime.time.fromisoformat('13:42:35.458+09:00'))
