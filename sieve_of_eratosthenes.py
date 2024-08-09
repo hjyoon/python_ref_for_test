10f29fb05c9ef085b4d0ea2acdf18f8e4b1e7b15
hjyoon <hjyoon@kakao.com>
2024-08-09 11:49:24 +0900

    update

--- sieve_of_eratosthenes.py
+++ sieve_of_eratosthenes.py
@@ -3,6 +3,7 @@ def sieve_of_eratosthenes(n):
     for k in range(2, int(n ** 0.5 + 1.5)):
         if seive[k]:
             seive[k*2::k] = [False] * ((n - k) // k)
-    return [x for x in range(n+1) if seive[x]]
+    return seive, [x for x in range(n+1) if seive[x]]
 
-print(sieve_of_eratosthenes(101)) # 에라토스테네스의 체
+
+print(sieve_of_eratosthenes(101))  # 에라토스테네스의 체
