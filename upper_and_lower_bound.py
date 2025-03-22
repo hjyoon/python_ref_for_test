1d967297fd07cd08b0be0b339bdaad5200779710
hjyoon <hjyoon@kakao.com>
2025-03-23 06:24:38 +0900

    Update upper_and_lower_bound.py

--- upper_and_lower_bound.py
+++ upper_and_lower_bound.py
@@ -1,8 +1,11 @@
+import bisect
+
+
 def upper_bound(s, e, d, l):
     while e - s > 0:
-        m = (s+e)//2
+        m = (s + e) // 2
         if l[m] <= d:
-            s = m+1
+            s = m + 1
         else:
             e = m
     return e
@@ -10,9 +13,9 @@ def upper_bound(s, e, d, l):
 
 def lower_bound(s, e, d, l):
     while e - s > 0:
-        m = (s+e)//2
+        m = (s + e) // 2
         if l[m] < d:
-            s = m+1
+            s = m + 1
         else:
             e = m
     return e
@@ -20,5 +23,8 @@ def lower_bound(s, e, d, l):
 
 L = [10, 10, 20, 20, 20, 30, 30, 40, 40, 40, 40, 50, 50, 60]
 
-print(lower_bound(0, len(L), 10, L))
 print(upper_bound(0, len(L), 60, L))
+print(lower_bound(0, len(L), 10, L))
+
+print(bisect.bisect_right(L, 60))
+print(bisect.bisect_left(L, 10))
